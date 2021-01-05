faf42d42a11de717244a602cf8177de9774f0971
Erno <hentonen@gmail.com>
2021-01-05 20:23:55 +0200

    backend structure 1

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const PORT = process.env.PORT | 3001;
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.listen(PORT);
+
+console.log("Running in port:"+PORT);
+
